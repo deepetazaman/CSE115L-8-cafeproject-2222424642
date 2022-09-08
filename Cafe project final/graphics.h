961f49fff32106768767584254b34743157e95f7
deepetazaman <deepeta.orpa@northsouth.edu>
2022-09-08 12:16:59 +0600

    file created

---
+++ Cafe project final/graphics.h
@@ -0,0 +1,6 @@
+int main()
+{
+    intwindow(500,400,"Image window");
+    readimagefile("coffee.jpg",0,0,500,300);
+    getch();
+}
---
+++ Cafe project final/splash.h
@@ -0,0 +1,29 @@
+void splash(void)
+{
+    printf("o.oOOOoo                              oOoOOoOOo o                  \n");
+    printf(" o     o                                  o     O                  \n");
+    printf(" O     O                                  o     o              O   \n");
+    printf(" oOooOO.                                  O     O             oOo  \n");
+    printf(" o     `O `OoOo. .oOo. 'o     O           o     OoOo. .oOoO'   o   \n");
+    printf(" O      o  o     OooO'  O  o  o           O     o   o O   o    O   \n");
+    printf(" o     .O  O     O      o  O  O           O     o   O o   O    o   \n");
+    printf(" 0OooOO'   o     `OoO'  `Oo'oO'           o     O   o `OoO'o   `oO \n");
+
+
+    printf("                                                                             .-. .-.              .                              \n");
+    printf("                                                                             |   |                |                              \n");
+    printf("                                                                    .-. .-. -|- -|-  .-.      .--.|--.  .-. .,-.        .  - -.  \n");
+    printf("                                                                   (   (   ) |   |  (.-'      `--.|  | (   )|   )     .'       ) \n");
+    printf("                            )   (                                   `-' `-'  '   '   `--'     `--''  `- `-' |`-'    .'      --:  \n");
+    printf("                           (    ) )                                                                         |        `.        ) \n");
+    printf("                             ) ( (                                                                          '          `.  - -'  \n");
+    printf("                           _______)_                 \n");
+    printf("                        .-'---------|                \n");
+    printf("                       ( c|.........|                \n");
+    printf("                        '-.||||||||||                \n");
+    printf("                           '________'                \n");
+    printf("                            '------'                 \n");
+    sleep(3);
+    system("cls");
+
+}
